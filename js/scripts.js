4b7daf2026e01f7eaac93151bbcefea0f979e736
qh <qianhu@pindous.com>
2018-01-11 09:36:41 -0800

    add all forms and interactions with JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
   $("#formOne").submit(function () {
     var appetizerInput = $("#appetizer").val();
+    var mainCourseInput = $("#mainCourse").val();
+    var dessertInput = $("#dessert").val();
 
     $(".appetizer").text(appetizerInput);
-
+    $(".mainCourse").text(mainCourseInput);
+    $(".dessert").text(dessertInput);
+    
     $("#result").show();
 
     event.preventDefault();
