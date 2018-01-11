068ecbdbb6c952baabd9d7abf59b2b7f85d2f640
qh <qianhu@pindous.com>
2018-01-11 09:59:54 -0800

    try don't repeat rule to rewritr JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,30 @@
-$(document).ready(function () {
-  $("#formOne").submit(function () {
-    var appetizerInput = $("#appetizer").val();
-    var mainCourseInput = $("#mainCourse").val();
-    var dessertInput = $("#dessert").val();
+// $(document).ready(function () {
+//   $("#formOne").submit(function (event) {
+//     var appetizerInput = $("#appetizer").val();
+//     var mainCourseInput = $("#mainCourse").val();
+//     var dessertInput = $("#dessert").val();
+//
+//     $(".appetizer").text(appetizerInput);
+//     $(".mainCourse").text(mainCourseInput);
+//     $(".dessert").text(dessertInput);
+//
+//     $("#result").show();
+//
+//     event.preventDefault();
+//   });
+// });
 
-    $(".appetizer").text(appetizerInput);
-    $(".mainCourse").text(mainCourseInput);
-    $(".dessert").text(dessertInput);
-    
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var blanks = ["appetizer", "mainCourse", "dessert"];
+    // these "appetizer", "mainCourse", "dessert" are for to form the previous var appetizerInput...
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      // take (var appetizerInput = $("#appetizer").val();) as refrence
+      $("." + blank).text(userInput).val();
+    });
+
     $("#result").show();
 
     event.preventDefault();
